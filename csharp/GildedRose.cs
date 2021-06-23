51d84b603cb7354a5a6da439f858efe364b0f34c
jarjanazy <celil.cercenazi@gmail.com>
2021-06-23 22:14:37 +0300

    Partially refactor some parts

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,47 +14,18 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
+                if (WillDecreaseQuality(i))
+                    Items[i].Quality = Items[i].Quality - 1;
+                
+                else if (Items[i].Quality < 50)
+                    IncreaseQuality(i);
+                
 
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
+
 
                 if (Items[i].SellIn < 0)
                 {
@@ -83,7 +54,39 @@ namespace csharp
                         }
                     }
                 }
+
             }
+        }
+
+        private void IncreaseQuality(int i)
+        {
+            Items[i].Quality += 1;
+            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (WillIncreaseQualityCase1(i))
+                    Items[i].Quality += 1;
+
+                if (WillIncreaseQualityCase2(i))
+                    Items[i].Quality += 1;
+            }
+        }
+
+        private bool WillIncreaseQualityCase2(int i)
+        {
+            return Items[i].SellIn < 6 && Items[i].Quality < 50;
+        }
+
+        private bool WillIncreaseQualityCase1(int i)
+        {
+            return Items[i].SellIn < 11 && Items[i].Quality < 50;
+        }
+
+        private bool WillDecreaseQuality(int i)
+        {
+            return Items[i].Name != "Aged Brie" && 
+                   Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && 
+                   Items[i].Quality > 0 && 
+                   Items[i].Name != "Sulfuras, Hand of Ragnaros";
         }
     }
 }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -9,48 +9,41 @@ namespace csharp
         {
             Console.WriteLine("OMGHAI!");
 
-            IList<Item> Items = new List<Item>{
-                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 15,
-                    Quality = 20
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 10,
-                    Quality = 49
-                },
-                new Item
-                {
-                    Name = "Backstage passes to a TAFKAL80ETC concert",
-                    SellIn = 5,
-                    Quality = 49
-                },
-				// this conjured item does not work properly yet
-				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-            };
+            IList<Item> Items = CreateItems();
 
             var app = new GildedRose(Items);
 
 
             for (var i = 0; i < 31; i++)
             {
-                Console.WriteLine("-------- day " + i + " --------");
-                Console.WriteLine("name, sellIn, quality");
-                for (var j = 0; j < Items.Count; j++)
-                {
-                    System.Console.WriteLine(Items[j]);
-                }
-                Console.WriteLine("");
+                WriteToConsole(i, Items);
                 app.UpdateQuality();
             }
+        }
+
+        private static void WriteToConsole(int i, IList<Item> items)
+        {
+            Console.WriteLine("-------- day " + i + " --------");
+            Console.WriteLine("name, sellIn, quality");
+            foreach (var item in items)
+                System.Console.WriteLine(item);
+
+            Console.WriteLine("");
+        }
+
+        private static List<Item> CreateItems()
+        {
+            return new List<Item>{
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 15, Quality = 20},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 49},
+                new Item {Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 49},
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
+            };
         }
     }
 }
