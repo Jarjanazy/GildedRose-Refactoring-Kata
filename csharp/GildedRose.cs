75b3f0bb1008f4b2b651c70d26cc0bccfdbfe776
jarjanazy <celil.cercenazi@gmail.com>
2021-06-24 19:14:42 +0300

    Refactor the code

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,79 +14,72 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (WillDecreaseQuality(i))
-                    Items[i].Quality = Items[i].Quality - 1;
+                ChangeQualityCase1(Items[i]);
                 
-                else if (Items[i].Quality < 50)
-                    IncreaseQuality(i);
-                
-
                 if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
+                    Items[i].SellIn -= 1;
 
                 if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-
+                    ChangeQualityCase2(Items[i]);
             }
         }
 
-        private void IncreaseQuality(int i)
+        private void ChangeQualityCase2(Item item)
         {
-            Items[i].Quality += 1;
-            if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+            if (item.Name != "Aged Brie")
             {
-                if (WillIncreaseQualityCase1(i))
-                    Items[i].Quality += 1;
+                if (WillDecreaseQualityCase2(item))
+                    item.Quality -= 1;
+                else
+                    item.Quality = 0;
+            }
+            else if (item.Quality < 50)
+                item.Quality += 1;
+        }
 
-                if (WillIncreaseQualityCase2(i))
-                    Items[i].Quality += 1;
+        private bool WillDecreaseQualityCase2(Item item)
+        {
+            return item.Quality > 0 && item.Name != "Sulfuras, Hand of Ragnaros" && item.Name != "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        private void ChangeQualityCase1(Item item)
+        {
+            if (WillDecreaseQualityCase1(item))
+                item.Quality -= 1;
+
+            else if (item.Quality < 50)
+                IncreaseQuality(item);
+        }
+
+        private void IncreaseQuality(Item item)
+        {
+            item.Quality += 1;
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (WillIncreaseQualityCase1(item))
+                    item.Quality += 1;
+
+                if (WillIncreaseQualityCase2(item))
+                    item.Quality += 1;
             }
         }
 
-        private bool WillIncreaseQualityCase2(int i)
+        private bool WillIncreaseQualityCase2(Item item)
         {
-            return Items[i].SellIn < 6 && Items[i].Quality < 50;
+            return item.SellIn < 6 && item.Quality < 50;
         }
 
-        private bool WillIncreaseQualityCase1(int i)
+        private bool WillIncreaseQualityCase1(Item item)
         {
-            return Items[i].SellIn < 11 && Items[i].Quality < 50;
+            return item.SellIn < 11 && item.Quality < 50;
         }
 
-        private bool WillDecreaseQuality(int i)
+        private bool WillDecreaseQualityCase1(Item item)
         {
-            return Items[i].Name != "Aged Brie" && 
-                   Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" && 
-                   Items[i].Quality > 0 && 
-                   Items[i].Name != "Sulfuras, Hand of Ragnaros";
+            return item.Name != "Aged Brie" &&
+                   item.Name != "Backstage passes to a TAFKAL80ETC concert" &&
+                   item.Quality > 0 &&
+                   item.Name != "Sulfuras, Hand of Ragnaros";
         }
     }
 }
